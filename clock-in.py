d8c801b455d67a599de4af83c3d76f104d156d9f
onion-quan <jinquan.shi@yale.edu>
2022-07-08 22:31:27 +0800

    Add 'internship'

--- clock-in.py
+++ clock-in.py
@@ -119,6 +119,7 @@ class ClockIn(object):
         new_info['gwszdd'] = ""
         new_info['szgjcs'] = ""
         # new_info['verifyCode'] = verifyCode
+        new_info['internship'] = 1
 
         # 2021.08.05 Fix 2
         magics = re.findall(r'"([0-9a-f]{32})":\s*"([^\"]+)"', html)
